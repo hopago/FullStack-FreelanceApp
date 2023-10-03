57e6a26108d6fc81d0e98eb98a1b01ef4d5b488e
Hopago <132269386+hopago@users.noreply.github.com>
2023-10-03 18:48:55 +0900

    Add files via upload

--- api/controllers/message.js
+++ api/controllers/message.js
@@ -1,3 +1,38 @@
-export const fn = async (req, res) => {
+import Message from '../models/Message.js';
+import Conversation from '../models/Conversation.js';
 
-}
+export const createMessage = async (req, res, next) => {
+    const newMessage = new Message({
+        conversationsId: req.body.conversationsId,
+        userId: req.userId,
+        desc: req.body.desc,
+    });
+    try {
+        const savedMessage = await newMessage.save();
+        await Conversation.findOneAndUpdate({
+            id: req.body.conversationsId
+        }, {
+            $set: {
+                readBySeller: req.isSeller,
+                readByBuyer: !req.isSeller,
+                lastMessage: req.body.desc,
+            }
+        }, {
+            new: true
+        });
+        res.status(200).json(savedMessage);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const getMessages = async (req, res, next) => {
+    try {
+        const messages = await Message.find({
+            conversationsId: req.params.conversationId
+        });
+        res.status(200).json(messages);
+    } catch (err) {
+        next(err);
+    }
+};
